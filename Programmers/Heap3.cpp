3320a0e58c214530f5e366b66c73cf00612d649a
somi014 <somi112005@gmail.com>
2022-08-14 14:27:32 +0900

    prority_queue 연습, map 연습 주석 제거, 프로그래머스 이중우선순위 큐 풀이

--- Programmers/Heap3.cpp
+++ Programmers/Heap3.cpp
@@ -1,12 +1,94 @@
-#include <iostream>
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <set>
 using namespace std;
 
-int solution(vector<vector<int>> jobs) {
-    int answer = 0;
+vector<int> solution(vector<string> operations) {
+	vector<int> answer;
+
+	vector<pair<string, int>> oper;
+	for (int i = 0; i < operations.size(); i++)
+	{
+		string s1 = operations[i].substr(0, 1);
+		int s2 = stoi(operations[i].substr(2, operations[i].size()));
+
+		oper.push_back(make_pair(s1, s2));
+	}
+
+	deque<int> pq;
+	for (int i = 0; i < oper.size(); i++)
+	{
+		if (oper[i].first == "I")
+		{
+			pq.push_back(oper[i].second);
+			sort(pq.begin(), pq.end());
+		}
+		else
+		{
+			if (pq.empty() == true)
+			{
+				continue;
+			}
+
+			if (oper[i].second > 0)
+			{
+				pq.pop_back();
+			}
+			else
+			{
+				pq.pop_front();
+			}
+		}
+	}
+
+	if (pq.size() > 0)
+	{
+		answer.push_back(pq.back());
+		answer.push_back(pq.front());
+	}
+	else
+	{
+		answer.push_back(0);
+		answer.push_back(0);
+	}
+
+	return answer;
+}
+
+vector<int> solution2(vector<string> arguments) {
+	vector<int> answer;
+	multiset<int> que;
+	multiset<int>::iterator iter;
+	string sub;
+
+	for (auto s : arguments) {
+		sub = s.substr(0, 2);
+		if (sub == "I ") que.insert(stoi(s.substr(2, s.length() - 2)));
+		else if (s.substr(2, 1) == "1" && que.size() > 0) { que.erase(--que.end()); }
+		else if (que.size() > 0) { que.erase(que.begin()); }
+	}
+
+	if (que.size() == 0) { answer.push_back(0); answer.push_back(0); }
+	else {
+		iter = --que.end(); answer.push_back(*iter);
+		iter = que.begin(); answer.push_back(*iter);
+	}
+
+	return answer;
+}
 
 
-    return answer;
+int main()
+{
+	vector<string> operations = { "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" };
+	//vector<string> operations = { "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" };
+	//vector<string> operations = { "I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6" };
+	//vector<string> operations = { "I 4", "I -1", "I 6", "I 3" };
+	solution2(operations);
+
+
+	return 0;
 }
---
+++ Programmers/PriorityQueueStudy.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Student
+{
+	int id;
+	string name;
+
+	Student(int _id, string _name)
+	{
+		id = _id;
+		name = _name;
+	}
+
+	bool operator<(const Student p) const
+	{
+		return this->id > p.id;
+	}
+};
+
+struct cmp
+{
+	bool operator()(Student a, Student b)
+	{
+		return a.id > b.id;
+	}
+};
+
+int main()
+{
+	priority_queue<int, vector<int>, less<int>> pq1;
+	pq1.push(1);
+	pq1.push(3);
+	pq1.push(2);
+	pq1.push(4);
+
+	while (pq1.size() > 0)
+	{
+		cout << pq1.top() << endl;
+		pq1.pop();
+	}
+
+	priority_queue<Student, vector<Student>, cmp> pq2;
+	pq2.push(Student(2, "나"));
+	pq2.push(Student(5, "마"));
+	pq2.push(Student(3, "다"));
+	pq2.push(Student(1, "가"));
+	pq2.push(Student(4, "라"));
+
+	while (pq2.size()> 0)
+	{
+		cout << "id : " << pq2.top().id  << " name : " << pq2.top().name << endl;
+		pq2.pop();
+	}
+
+	return 0;
+}
