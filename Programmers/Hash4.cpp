851abbf9891c02606d69633f0fe28b840323ce63
somi014 <somi112005@gmail.com>
2022-11-27 21:52:33 +0900

    프로그래머스 폰켓몬 추가

---
+++ Programmers/Hash4.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	int answer = 0;
+
+	map<int, int> type;
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		type[nums[i]]++;
+	}
+
+	for (int i = 0; i < type.size(); i++)
+	{
+		answer++;
+		if (answer >= nums.size() / 2)
+		{
+			break;
+		}
+	}
+
+	//if (nums.size() / 2 > type.size())
+	//{
+	//	answer = nums.size() / 2;
+	//	cout << "answer0  : " << answer << endl;
+	//}
+	//else
+	//{
+	//	answer = type.size();
+	//}
+		cout << "answer1  : " << answer << endl;
+
+	return answer;
+}
+
+int main()
+{
+	//vector<int> nums = { 3,1,2,3 };
+	vector<int> nums = { 3,3,3,2,2,4 };
+	//vector<int> nums = { 3,3,3,2,2,2 };
+	//vector<int> nums = { 1,2,3,4,5,2 };
+	solution(nums);
+}
