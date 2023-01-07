338b34453d1d24a3b01598663ef55bef5fcb8b89
somi014 <somi112005@gmail.com>
2023-01-07 22:13:13 +0900

    프로그래머스 2레벨 숫자의 표현

---
+++ Programmers/Lv2_5.cpp
@@ -0,0 +1,45 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/// <summary>
+/// 프로그래머스 c++ 2레벨 숫자의 표현
+/// </summary>
+
+int solution(int n) {
+    int answer = 0;
+
+    int sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        sum = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            sum += j;
+
+            if (sum == n)
+            {
+                answer++;
+                break;
+            }
+            else if (sum > n)
+            {
+                break;
+            }
+        }
+    }
+
+    return answer + 1;
+}
+
+int main()
+{
+    int n = 12;
+    
+    cout << "solution " << solution(n) << endl;
+
+
+    return 0;
+}
