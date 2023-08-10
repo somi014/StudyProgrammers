31f45e7eb7fa09f0e52437c5b860f09d41c674f5
somi014 <somi112005@gmail.com>
2023-08-10 16:29:17 +0900

    프로그래머스 2017 팁스타운 예상 대진법 문제

---
+++ Programmers/TipsTown2017.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+
+    int abs_num = 0;
+    bool stop = false;
+
+    while (stop == false)
+    {
+        a = (a / 2) + (a % 2);          // 1과 2번이 붙었을 때 이긴사람은 1이 되도록 만든다
+        b = (b / 2) + (b % 2);
+
+        abs_num = abs(a - b);           //a와 b가 일련의 숫자가 되었을 때 맞붙을 수 있다
+        if (abs_num == 0)
+        {
+            stop = true;
+        }
+        answer++;
+    }
+
+    return answer;
+}
+
+
+int main()
+{
+    cout << solution(8, 4, 7) << endl;
+
+    return 0;
+}
